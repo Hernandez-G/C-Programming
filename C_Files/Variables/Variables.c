410ea540128ec7a7ec00d3f4546a3f67bfc31c9d
hernandez-g <g5hernandez@yahoo.com>
2022-07-14 13:19:30 -0700

    variable placeholders

--- C_Files/Variables/Variables.c
+++ C_Files/Variables/Variables.c
@@ -2,7 +2,20 @@
 
 int main() 
 {
-    /* code */
+    int x; //declaration
+    x = 123; //initialization 
+    int y = 321; //Declaration && initialization
+    int age = 21;  //intger
+    float gpa = 3.21; //floating point num
+    char grade = 'A'; //single char > in ' ' 
+    char name[] = "Bro"; //array of characters
+
+    printf("You are %d years old\n", age);  //%d is decimal placeholer, [variable]
+    printf("Hello %s\n", name);  //%s is string placeholer, [variable]
+    printf("Your average grade is %c\n", grade);  //%s is character placeholer, [variable]
+    printf("Your average gpa is %f\n", gpa);  //%s is float placeholer, [variable]
+
+
     return 0;
 }
 
@@ -10,5 +23,6 @@ int main()
 //Variables:
     /*  space in memory to store the value.
         variables act like the stored variables
-        when declared
+        when declared.
+        First, need to declare the data type of var.
     */
