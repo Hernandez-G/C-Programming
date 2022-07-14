a5346b592f736528e555ebc6e86d00d48839dbd8
hernandez-g <g5hernandez@yahoo.com>
2022-07-14 12:59:39 -0700

    compiling files

---
+++ C_Files/HelloWorld.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello World!\n");
+    printf("Gaby\n");
+    return 0;
+    //like a period-- ending the sentence
+    //if theres an error, will return 1
+}
+
+//Compiling files:
+    //find file location of C file
+    //copy location
+    //Complie file: gcc [name of file compiling]
+    //run 
+    //When running the code while in working directory add PREFIX: "./" in front of a.out/a.exe file
+    //FOR LINUX/Ubuntu
+    //cd code/LearnC/C_Files
+
