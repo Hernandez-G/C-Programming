a14c096b37c8ad1eb7d2ece8893f0cb99b08f5b7
hernandez-g <g5hernandez@yahoo.com>
2022-07-14 16:35:07 -0700

    Format Specifiers

---
+++ C_Files/FormatSpecifiers/FormatSpecifiers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+}
+
+//FORMAT SPECIFIERS
+    /*defines and formats types of displayed data
+        %c = char
+        %s = string (array of char)
+        %f = float
+        %fl = double
+        %d = integer
+
+        %.1 = decimal precision
+        %1 = minimum field width
+        %- = left align
+    */
+        
