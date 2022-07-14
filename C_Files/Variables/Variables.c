ba52621e02c6984b99920881dd3512336f62ddc9
hernandez-g <g5hernandez@yahoo.com>
2022-07-14 13:05:52 -0700

    variables

---
+++ C_Files/Variables/Variables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() 
+{
+    /* code */
+    return 0;
+}
+
+
+//Variables:
+    /*  space in memory to store the value.
+        variables act like the stored variables
+        when declared
+    */
