2beef25cd3c388a8b4550c5d4dcb255f1a16707c
hernandez-g <g5hernandez@yahoo.com>
2022-07-14 19:41:58 -0700

    Format Specifiers finished

--- C_Files/FormatSpecifiers/FormatSpecifiers.c
+++ C_Files/FormatSpecifiers/FormatSpecifiers.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
 int main() {
+    float item1 = 5.75;
+    float item2 = 10.00;
+    float item3 = 100.99;
+
+    printf("item1: $%8.2f\n", item1);
+    printf("item2: $%8.2f\n", item2);
+    printf("item3: $%8.2f\n", item3);
 
 }
 
