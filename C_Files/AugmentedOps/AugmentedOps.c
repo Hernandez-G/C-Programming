6da187499c0523b9ff4b501dda3d9d6a7f76744f
hernandez-g <g5hernandez@yahoo.com>
2022-07-15 15:33:26 -0700

    Augmented Ops

---
+++ C_Files/AugmentedOps/AugmentedOps.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    int x = 10;
+    // x+= 2;
+    x = x + 2;
+
+    return 0;
+}
+
+//AUGMENTED ASSIGNMENT OPERATORS
+    /* used to replace a statement where an operator takes
+        a variable as one of its arguments and assigns the result
+        back to the same variable.
+    */
