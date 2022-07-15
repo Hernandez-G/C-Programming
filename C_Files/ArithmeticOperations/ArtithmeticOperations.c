e09ee59574f33cf839f08694f2b49d1c7f50273d
hernandez-g <g5hernandez@yahoo.com>
2022-07-14 20:54:40 -0700

    Arithmetic Operations

---
+++ C_Files/ArithmeticOperations/ArtithmeticOperations.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int x = 5;
+    int y = 2;
+    // float z = x / (float) y; 
+
+    // printf("%f", z);
+    
+// MODULUS
+    // int z = x % y;
+    // printf("%d", z);
+    
+// increment
+    x++; 
+    y--; 
+    printf("%d", y);
+
+    return 0;
+}
+
+//reminder: int does not print out decimals
+// int > float > float = ... + (float) ... 
+
+// ARITHMETIC OPERATORS
+    /* 
+        + > addition
+        - > Subtraction
+        * > Multiplication
+        / > Division
+        % > Modulus
+        ++ > Increment
+        -- > Decrement
+    */
