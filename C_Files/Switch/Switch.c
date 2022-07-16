714c037d1737ea9615eaadc75a17adc210ef2819
hernandez-g <g5hernandez@yahoo.com>
2022-07-15 18:25:37 -0700

    switch statements

---
+++ C_Files/Switch/Switch.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+    return 0;
+}
+
+// SWITCH STATEMENTS:
+/* an alternative way to use many "else if" statements that lets a value
+    to be tested for equality against many cases 
+*/
