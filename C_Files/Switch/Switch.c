c5da7f03500f66fa92af345f7406fd56f6256794
hernandez-g <g5hernandez@yahoo.com>
2022-07-15 18:32:46 -0700

    Switch Statement

--- C_Files/Switch/Switch.c
+++ C_Files/Switch/Switch.c
@@ -2,10 +2,36 @@
 
 int main() {
 
+    char grade;
+
+    printf("\nEnter a letter grade: ");
+    scanf("%c", &grade);
+
+    switch(grade) {
+        case 'A':
+            printf("Perfect!");
+            break;
+        case 'B':
+            printf("Good!");
+            break;
+        case 'C':
+            printf("Mid!");
+            break;
+        case 'D':
+            printf("Almost an F");
+            break;
+        case 'F':
+            printf("Fail");
+            break;
+        default:
+            printf("Please enter only valid grades");
+        
+
+    }
+
     return 0;
 }
 
-// SWITCH STATEMENTS:
-/* an alternative way to use many "else if" statements that lets a value
-    to be tested for equality against many cases 
-*/
+
+/* Switch Statements: an alternative way to use many "else if" statements */
+// breaks exist so all of the "printf's" dont print when executed
