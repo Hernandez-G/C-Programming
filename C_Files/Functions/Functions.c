2f59762d9c9dcb0eed7ad77d6aceca8cea403ff9
hernandez-g <g5hernandez@yahoo.com>
2022-07-16 18:11:50 -0700

    basic functions

---
+++ C_Files/Functions/Functions.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// Birthday function 
+// call void!
+void birthday() {
+    printf("\nHappy Birthday to you!");
+    printf("\nHappy Birthday to you!");
+    printf("\nHappy Birthday dear... you!");
+    printf("\nHappy Birthday to you!");
+
+}
+
+
+int main() {
+// instead of writing it x3, just execute x3 "birthday()"
+    birthday();
+    birthday();
+    birthday();
+
+    return 0;
+}
