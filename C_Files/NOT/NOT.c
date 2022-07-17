c1d33c920f9b621f1fee292c5cd649ba5296bf33
hernandez-g <g5hernandez@yahoo.com>
2022-07-16 18:06:23 -0700

    NOT Op

---
+++ C_Files/NOT/NOT.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    bool sunny = false;
+
+    if(!sunny) {
+        printf("\n Its not sunny outside");
+    }else {
+
+        printf("\n Its sunny outside");
+    }
+
+
+    return 0;
+}
+
+// (!) NOT = reverses the state of a condition 
