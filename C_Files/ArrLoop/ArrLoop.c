9a0a3ea30bc87a95eef0232dc337359354b05b8d
hernandez-g <g5hernandez@yahoo.com>
2022-07-18 00:06:23 -0700

    iterating through an array of prices using a for loop

---
+++ C_Files/ArrLoop/ArrLoop.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() 
+{
+    double prices[] = {5.0, 10.00, 15.00, 25.00, 20.00, 30.00};
+
+
+
+// EXAMPLE ONE:
+    // for(int i = 0; i < 5; i++) 
+    // {
+
+    // printf("$%.2lf\n", prices[i]);
+    // }
+
+// EXAMPLE TWO: using sizeof
+    // printf("%d bytes", sizeof(prices));
+
+    for(int i = 0; i < sizeof(prices)/sizeof(prices[0]); i++) 
+    {
+
+    printf("$%.2lf\n", prices[i]);
+    }
+
+
+    return 0;
+}
