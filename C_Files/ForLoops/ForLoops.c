80fd37ab5fcce55b38994998b6815ded8694c085
hernandez-g <g5hernandez@yahoo.com>
2022-07-17 18:03:50 -0700

    for loop finished

--- C_Files/ForLoops/ForLoops.c
+++ C_Files/ForLoops/ForLoops.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 
 int main() {
+    for(int i = 10; i >= 1; i-= 2) 
+    {
+        printf("%d\n", i);
+
+    }
 
 
     return 0;
 }
 
 // FOR LOOPS = repeats a section of code a limited amount of times
+
+//for(index, condition, increment/decrement)
+//for(index, condition, i++/i--/i-=num/i*=num)
