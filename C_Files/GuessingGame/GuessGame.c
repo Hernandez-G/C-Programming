acfb2ee58092a306abd6b3ac1f82667eaa3bb600
hernandez-g <g5hernandez@yahoo.com>
2022-07-18 20:05:18 -0700

    Guess Game

--- C_Files/GuessingGame/GuessGame.c
+++ C_Files/GuessingGame/GuessGame.c
@@ -2,47 +2,40 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main() 
+int main()
 {
-    const int MIN = 1;
-    const int MAX = 50;
-    int guess;
-    int guesses;
-    int answer;
+   const int MIN = 1;
+   const int MAX = 100;
+   int guess;
+   int guesses;
+   int answer;
 
-    srand(time(0));
-// generates random number 1 - 50
-    answer = (rand() % MAX) + MIN;
+   //uses the current time as a seed
+   srand(time(0));
+   
+   //generate a random number between MIN & MAX
+   answer = (rand() % MAX) + MIN;
 
+   do{
+      printf("Enter a guess: ");
+      scanf("%d", &guess);
+      if(guess > answer)
+      {
+         printf("Too high!\n");
+      }
+      else if(guess < answer)
+      {
+         printf("Too low!\n");
+      }
+      else{
+         printf("CORRECT!\n");
+      }
+      guesses++;
+      }while(guess != answer);
 
-
-do
-    {
-    printf("Enter a guess: ");
-    scanf("%d", &guess);
-
-    
-    if(guess > answer)
-    {
-        printf("Too high!\n");
-    }
-    else if(guess < answer)
-    {
-
-        printf("Too low!\n");
-
-    } 
-    else 
-    {
-
-        printf("Correcto-MUNDO!\n");
-    }
-    guesses++;
-    
-}while(guess != answer);
-
-        printf("answer: %d\n", answer);
-        printf("guesses: %d\n", guesses);
-
-    return 0;
+   printf("**********************\n");
+   printf("answer: %d\n", answer);
+   printf("**********************");
+ 
+   return 0;
 }
